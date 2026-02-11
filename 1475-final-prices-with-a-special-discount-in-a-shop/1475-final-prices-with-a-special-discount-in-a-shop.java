88dc37a13614c5601b4cd092f9f8f5d77526c670
pavan-56-kalyan <vtu24943@veltech.edu.in>
2026-02-11 10:38:09 +0530

    Time: 4 ms (51.81%), Space: 45.5 MB (21.65%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int n = prices.length;
+        int[] result = Arrays.copyOf(prices, n);
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] >= prices[i]) {
+                int idx = stack.pop();
+                result[idx] = prices[idx] - prices[i];
+            }
+            stack.push(i);
+        }
+        return result;        
+    }
+}
